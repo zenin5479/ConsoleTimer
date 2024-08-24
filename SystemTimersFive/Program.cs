fa409dda4092d86ee9de7c7549fc302b8d94980b
zenin5479 <zenin5479@gmail.com>
2024-08-24 18:30:42 +0300

    Update 0.6 Обновлен метод

---
+++ SystemTimersFive/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Timers;
+
+// System.Timers
+
+namespace SystemTimersFive
+{
+   internal class Program
+   {
+      static void Main()
+      {
+         // Утилизация таймеров
+         // Компонент Timer реализует интерфейс IDisposable
+         // В большинстве случаев экземпляры таймера в приложениях будут автоматически удаляться, когда они выходят из области действия
+         // Однако в некоторых случаях приходится делать это вручную
+         using (Timer timer = new Timer(1000))
+         {
+            timer.Elapsed += (sender, eventArgs) =>
+            {
+               Console.WriteLine($"Прошедшее событие в {eventArgs.SignalTime:G}");
+            };
+            timer.Start();
+            // Избегать отправки области действия таймера слишком рано
+            Console.ReadLine();
+         }
+
+         //Console.ReadLine();
+      }
+   }
+}
