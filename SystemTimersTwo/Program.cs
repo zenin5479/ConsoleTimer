c1f7d3030634ea3211db013a2376c0dcf23bd8af
zenin5479 <zenin5479@gmail.com>
2024-08-24 18:19:25 +0300

    Update 0.3 Обновлен метод

---
+++ SystemTimersTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Timers;
+
+// System.Timers
+
+namespace SystemTimersTwo
+{
+   internal class Program
+   {
+      static void Main()
+      {
+         // Генерация повторяющихся событий с помощью таймера
+         // Любой созданный таймер будет многократно вызывать события после запуска
+         // Это связано с тем, что свойство таймера AutoReset по умолчанию установлено в true
+         // Однако где нужен таймер для вызова события Elapsed только один раз,
+         // необходимо установить свойство в AutoReset = false
+         var timer = new Timer(1000);
+         timer.Elapsed += OnEventExecution;
+         // Отключить повторяющиеся события
+         timer.AutoReset = false;
+         timer.Start();
+
+         Console.ReadLine();
+      }
+
+      public static void OnEventExecution(object sender, ElapsedEventArgs eventArgs)
+      {
+         Console.WriteLine($"Прошедшее событие в {eventArgs.SignalTime:G}");
+      }
+   }
+}
